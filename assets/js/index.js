78af6d818467d26d483498803dd118bd6772b4a3
maipengin <mai.pengin@icloud.com>
2021-11-08 22:41:56 +0900

    背景修正

--- assets/js/index.js
+++ assets/js/index.js
@@ -37,22 +37,22 @@ function fadeAnime(){
   });// ここまで画面をスクロールをしたら動かしたい場合の記述
 
 
-  var parallaxBkImg = function(){
-    $(window).on('load resize', function() {
-      $(window).on('load scroll', function(){
-        var $winTop = $(window).scrollTop();
-        var $target = $('.p-bgImage');
-        var $winWidth = $(window).width();
-        if($winWidth < 736) {
-          $target.each(function(index){
-            var $position = $winTop - $target.eq(index).offset().top;
-            if($winTop > $target.eq(index).offset().top - 800) {
-              $target.eq(index).css({
-                'background-position-y': $position * .4
-              });
-            }
-          });
-        }
-      });
-    });
-  }();
+  // var parallaxBkImg = function(){
+  //   $(window).on('load resize', function() {
+  //     $(window).on('load scroll', function(){
+  //       var $winTop = $(window).scrollTop();
+  //       var $target = $('.p-bgImage');
+  //       var $winWidth = $(window).width();
+  //       if($winWidth < 736) {
+  //         $target.each(function(index){
+  //           var $position = $winTop - $target.eq(index).offset().top;
+  //           if($winTop > $target.eq(index).offset().top - 800) {
+  //             $target.eq(index).css({
+  //               'background-position-y': $position * .4
+  //             });
+  //           }
+  //         });
+  //       }
+  //     });
+  //   });
+  // }();
